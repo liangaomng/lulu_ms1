a0d26ef435697458e2864598f553a13ded99517d
liangaomng <liangaoming@foxmail.com>
2023-12-15 12:38:40 +0800

    12.15-3

--- Result/Expr1_/test1d.sh
+++
@@ -1,1 +0,0 @@
-python3 Expr1_run.py --expr_set_path "Expr1d/Expr_1.xlsx"
--- shell/expr1_.sh
+++ shell/expr1_.sh
@@ -2,6 +2,6 @@
 pip install pandas
 pip install openpyxl
 pip install xlrd==1.2.0
-cd ../
+
 #实验1
 python3 Expr1_run.py --expr_set_path "Expr1d/Expr_1.xlsx"
