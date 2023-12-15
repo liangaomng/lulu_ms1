77aa23f4a672c8ae820bc904c27d80f53991d4a6
liangaomng <liangaoming@foxmail.com>
2023-12-15 16:47:28 +0800

    12.15-7

--- shell/expr1_.sh
+++ shell/expr1_.sh
@@ -6,4 +6,14 @@ pip install captum
 #cd if you need
 #实验1
 python3 Expr1_run.py --expr_set_path "Expr1d/Expr_1.xlsx"
-python3 Expr1_run.py --expr_set_path "Expr1d/Expr_1.xlsx"
+python3 Expr1_run.py --expr_set_path "Expr1d/Expr_2.xlsx"
+python3 Expr1_run.py --expr_set_path "Expr1d/Expr_3.xlsx"
+python3 Expr1_run.py --expr_set_path "Expr1d/Expr_4.xlsx"
+python3 Expr1_run.py --expr_set_path "Expr1d/Expr_5.xlsx"
+print("residual")
+python3 Expr1_run.py --expr_set_path "Expr1d/Expr_6.xlsx"
+python3 Expr1_run.py --expr_set_path "Expr1d/Expr_7.xlsx"
+python3 Expr1_run.py --expr_set_path "Expr1d/Expr_7.xlsx"
+python3 Expr1_run.py --expr_set_path "Expr1d/Expr_8.xlsx"
+python3 Expr1_run.py --expr_set_path "Expr1d/Expr_9.xlsx"
+python3 Expr1_run.py --expr_set_path "Expr1d/Expr_10.xlsx"
